0dc55bbaba9077741242236aa17779a66cfd5858
Arham Malik <arhammalik900@yahoo.com>
2025-12-20 04:34:37 +0500

    searchBar

--- app/customer/(tabs)/search.jsx
+++ app/customer/(tabs)/search.jsx
@@ -1,14 +1,43 @@
-import { Text,View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Search = () => {
-  
+  const data = []; // Replace with search results
+
   return (
-     <SafeAreaView className="flex-1 bg-white">
-        <View className="flex-1 justify-center items-center">
-          <Text className="font-quicksand-bold text-3xl">Search Section</Text>
-        </View>
-      </SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white px-5 pt-5">
+      {/* Header */}
+      <View className="mb-5">
+        <Text className="uppercase text-primary font-quicksand-bold text-lg">
+          Menu
+        </Text>
+        <Text className="text-dark-100 font-quicksand-semibold mt-1">
+          Find your favourite food
+        </Text>
+      </View>
+
+   <SearchBar/>
+
+      {/* Search Results */}
+      {data.length === 0 ? (
+        <Text className="text-center mt-10 text-gray-500">
+          No Results Found!
+        </Text>
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={(item, index) => item.id?.toString() || index.toString()}
+          renderItem={({ item }) => (
+            <View className="mb-4 bg-gray-100 p-4 rounded-lg">
+              <Text className="text-neutral-800">{item.name}</Text>
+            </View>
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 120 }}
+        />
+      )}
+    </SafeAreaView>
   );
 };
 
---
+++ components/SearchBar.jsx
@@ -0,0 +1,32 @@
+import { images } from "@/constants";
+import { useState } from "react";
+import { Image, TextInput, TouchableOpacity, View } from "react-native";
+
+const SearchBar = () => {
+  const [query, setQuery] = useState("");
+
+  return (
+    <View className="flex-row items-center bg-white-100 rounded-3xl border-2 border-gray-300 px-3 py-2 mb-5">
+      <TextInput
+        className="flex-1 text-base text-white-100 px-2"
+        placeholder="Search for food items..."
+        placeholderTextColor="#A0A0A0"
+        value={query}
+        onChangeText={setQuery}
+        returnKeyType="search"
+      />
+
+      {/* Search Icon */}
+      <TouchableOpacity className="ml-2">
+        <Image
+          source={images.search}
+          className="w-6 h-6"
+          resizeMode="contain"
+          tintColor="#5D5F6D"
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchBar;
