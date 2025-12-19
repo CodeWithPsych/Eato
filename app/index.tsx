1626fbdcc116e536629d06ab1d6ee0262aef40fe
Arham Malik <arhammalik900@yahoo.com>
2025-12-19 17:43:13 +0500

    nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
-
+ 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome to FYP</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-center text-primary font-quicksand-bold">
+        Welcome to Eato !
+      </Text>
     </View>
   );
 }
